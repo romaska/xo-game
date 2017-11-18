72bf986c4958a64bca5f55bebc4803175e5e7268
romaska <r_omaska@mail.ru>
2017-11-19 01:41:52 +0300

    test

---
+++ containers/Square.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+var squareCss = {
+    width: 30,
+    height: 30,
+    border: "1px solid black"
+};
+
+
+export default Square extends Component{
+    render(){
+        return(
+            <div style={squareCss}>
+
+            </div>
+        )
+    }
+}
